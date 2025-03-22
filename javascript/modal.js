658113fc568a0680ae06aea01d23559a5f675940
EverAlexander <71670356+EverAlexander@users.noreply.github.com>
2025-03-22 17:25:36 -0600

    Add files via upload

---
+++ javascript/modal.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("#myBtn").click(function(){
+    $("#myModal").modal();
+  });
+});
+
+$(document).ready(function(){
+  $("#btn2").click(function(){
+    $("#modal1").modal();
+  });
+});
